db63c5de28254183ecd66b62872ef40d16eaac46
Shivang <32.shivangdubey@gmail.com>
2022-08-05 00:16:51 +0530

    Added shell script to manage client deployment

---
+++ update.sh
@@ -0,0 +1,13 @@
+# Abort on errors
+set -e
+
+# Remove the client directory if already exists
+sudo rm -rf client
+
+# Clone from frontend repo (branch: deploy) into client directory
+git clone --depth=1 --branch=deploy git@github.com:SHD-420/linkpreview-frontend.git client
+
+# Eliminate source control from client (directory)
+sudo rm -rf ./client/.git
+
+echo "Frontend updated!"
